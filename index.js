1ecd24e788fd172389d9c509cccf5fcaaff00930
TroyTae <tjy970721@gmail.com>
2020-02-06 12:07:10 +0900

    fix: lint

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
 // Process front matter and pass to cb
-//
 'use strict';
 
 module.exports = function front_matter_plugin(md, cb) {
   var min_markers = 3,
       marker_str  = '-',
       marker_char = marker_str.charCodeAt(0),
-      marker_len  = marker_str.length
+      marker_len  = marker_str.length;
 
   function frontMatter(state, startLine, endLine, silent) {
-    var pos, nextLine, marker_count, markup, token,
-        old_parent, old_line_max, start_content,
+    var pos,
+        nextLine,
+        marker_count,
+        token,
+        old_parent,
+        old_line_max,
+        start_content,
         auto_closed = false,
         start = state.bMarks[startLine] + state.tShift[startLine],
         max = state.eMarks[startLine];
 
     // Check out the first character of the first line quickly,
     // this should filter out non-front matter
-    //
-    if (startLine !== 0 || marker_char !== state.src.charCodeAt(0)) { return false; }
+    if (startLine !== 0 || marker_char !== state.src.charCodeAt(0)) {
+      return false;
+    }
 
     // Check out the rest of the marker string
-    //
-    for (pos = start + 1; pos <= max; pos++) { // while pos <= 3
+    // while pos <= 3
+    for (pos = start + 1; pos <= max; pos++) {
       if (marker_str[(pos - start) % marker_len] !== state.src[pos]) {
-        start_content = pos + 1
+        start_content = pos + 1;
         break;
       }
     }
 
     marker_count = Math.floor((pos - start) / marker_len);
 
-    if (marker_count < min_markers) { return false; }
+    if (marker_count < min_markers) {
+      return false;
+    }
     pos -= (pos - start) % marker_len;
 
     // Since start is found, we can report success here in validation mode
-    //
-    if (silent) { return true; }
+    if (silent) {
+      return true;
+    }
 
     // Search for the end of the block
-    //
     nextLine = startLine;
 
     for (;;) {
@@ -60,7 +67,9 @@ module.exports = function front_matter_plugin(md, cb) {
         break;
       }
 
-      if (marker_char !== state.src.charCodeAt(start)) { continue; }
+      if (marker_char !== state.src.charCodeAt(start)) {
+        continue;
+      }
 
       if (state.sCount[nextLine] - state.blkIndent >= 4) {
         // closing fence should be indented less than 4 spaces
@@ -74,13 +83,17 @@ module.exports = function front_matter_plugin(md, cb) {
       }
 
       // closing code fence must be at least as long as the opening one
-      if (Math.floor((pos - start) / marker_len) < marker_count) { continue; }
+      if (Math.floor((pos - start) / marker_len) < marker_count) {
+        continue;
+      }
 
       // make sure tail has spaces only
       pos -= (pos - start) % marker_len;
       pos = state.skipSpaces(pos);
 
-      if (pos < max) { continue; }
+      if (pos < max) {
+        continue;
+      }
 
       // found!
       auto_closed = true;
@@ -96,7 +109,7 @@ module.exports = function front_matter_plugin(md, cb) {
 
     token        = state.push('front_matter', null, 0);
     token.hidden = true;
-    token.markup = state.src.slice(startLine, pos)
+    token.markup = state.src.slice(startLine, pos);
     token.block  = true;
     token.map    = [ startLine, pos ];
     token.meta   = state.src.slice(start_content, start - 1);
@@ -110,7 +123,17 @@ module.exports = function front_matter_plugin(md, cb) {
     return true;
   }
 
-  md.block.ruler.before('table', 'front_matter', frontMatter, {
-    alt: [ 'paragraph', 'reference', 'blockquote', 'list' ]
-  });
+  md.block.ruler.before(
+    'table',
+    'front_matter',
+    frontMatter,
+    {
+      alt: [
+        'paragraph',
+        'reference',
+        'blockquote',
+        'list' 
+      ]
+    }
+  );
 };
