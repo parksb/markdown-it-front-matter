1c359a3077c30d8b439db558ddedd38c827cb2e2
Opportunity <opportunity@live.in>
2020-10-12 18:39:52 +0800

    Improve typings

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,9 @@
 declare module 'markdown-it-front-matter' {
   import MarkdownIt from 'markdown-it/lib'
-  export type FrontMatterPluginOptions = (rawMeta: string) => void
-  const markdownItFrontMatter: MarkdownIt.PluginWithOptions<FrontMatterPluginOptions>
+
+  namespace markdownItFrontMatter {
+    type FrontMatterPluginOptions = (rawMeta: string) => void
+  }
+  const markdownItFrontMatter: MarkdownIt.PluginWithOptions<markdownItFrontMatter.FrontMatterPluginOptions>
   export = markdownItFrontMatter
 }
