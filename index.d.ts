c8178edd90189c2523d521e5bf3b1a054200bc87
Philip Molares <git@molar.es>
2020-10-12 08:09:27 +0200

    Add type declaration (#12)

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module 'markdown-it-front-matter' {
+  import MarkdownIt from 'markdown-it/lib'
+  export type FrontMatterPluginOptions = (rawMeta: string) => void
+  const markdownItFrontMatter: MarkdownIt.PluginWithOptions<FrontMatterPluginOptions>
+  export = markdownItFrontMatter
+}
